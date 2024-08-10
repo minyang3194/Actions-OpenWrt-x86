8de0f7d70c2c396a9092f67bd603c2c7ad96e746
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-08-11 04:35:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,7 +29,7 @@ git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 make menuconfig
 
 #添加istore
-echo >> feeds.conf.default
+# echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 ./scripts/feeds update istore
 ./scripts/feeds install -d y -p istore luci-app-store
