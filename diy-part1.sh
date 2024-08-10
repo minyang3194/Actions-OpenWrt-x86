8ddf61ab81cfea5a20205bf5754b96a47e3fff6e
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-08-11 05:49:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf
 #
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git innmonkey https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default
