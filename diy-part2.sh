6a03cb7d04691812004077b629099fbdb978af48
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-08-20 01:03:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git innmonkey https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default
-
-   
+#echo 'src-git innmonkey https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default   
 echo 'src-git opentopd https://github.com/sirpdboy/sirpdboy-package;main' >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
