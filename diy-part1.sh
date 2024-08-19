923fc124463df4a2eeb9ab20bb11b1d849424dec
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-08-20 00:56:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,7 +27,7 @@ touch wget/DISTRIB_REVISION3
 touch files/usr/share/Check_Update.sh
 touch files/usr/share/Lenyu-auto.sh
 # sirpdboy源码
-src-git opentopd  https://github.com/sirpdboy/sirpdboy-package
+src-git opentopd https://github.com/sirpdboy/sirpdboy-package
 
 # MAC 地址与 IP 绑定
 cp -rf ../immortalwrt_luci/applications/luci-app-arpbind ./feeds/luci/applications/luci-app-arpbind
